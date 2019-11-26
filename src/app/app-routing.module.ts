0b21488f86c557fbabb7d7b74b10ac78fab7799c
root <root@ip-10-0-7-250.ap-southeast-1.compute.internal>
2019-11-26 11:34:16 +0000

    router file changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { UserComponent } from './user/user.component';
 import { LoginHistoryComponent } from './login-history/login-history.component';
 import { AboutComponent } from './about/about.component'
 const routes: Routes = [
+  {path: '', redirectTo: '/about', pathMatch: 'full'},
   {path: 'user', component: UserComponent},
   {path: 'loginhistory', component: LoginHistoryComponent},
   {path: 'about', component: AboutComponent},
